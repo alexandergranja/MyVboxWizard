af1b8d143657fcfc471baec6e3a1bfd764748af7
alexandergranja <alexandergranja@gmail.com>
2018-03-17 09:40:24 -0500

    creacion de de vname.sh

--- MiWizard/ncore.sh
+++ MiWizard/ncore.sh
@@ -6,7 +6,7 @@
 NUMCORES=$(cat /proc/cpuinfo | grep processor | wc -l) 
 NUMTHREAD=$(cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $4}')
 NUMCORES=$(( NUMCORES*NUMTHREAD/2 ))
-NCORE="dialog --backtitle \"Num CPU\" --radiolist \"Seleccione Numero de Cores:\" 10 40 4 1 1 on" 
+NCORE="dialog --backtitle \"Numero CPU\" --radiolist \"Seleccione Numero de Cores:\" 10 40 4 1 1 on" 
 for i in $(seq 2 1 ${NUMCORES}); do
 	NCORE="${NCORE} ${i} ${i} off"
 done
